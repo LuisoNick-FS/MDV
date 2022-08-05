d1b42b19f9b07f7c9469b7ddcf286b92e793a5bf
LuisoNick-FS <87244599+LuisoNick-FS@users.noreply.github.com>
2022-08-05 11:49:51 -0400

    work on api

---
+++ apiAssignment/apiAssignment/DisplayPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace apiAssignment
+{
+    public partial class DisplayPage : ContentPage
+    {
+        public DisplayPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
--- apiAssignment/apiAssignment/Models/DataManager.cs
+++ apiAssignment/apiAssignment/Models/DataManager.cs
@@ -1,10 +1,54 @@
 ﻿using System;
+using System.Net;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using Newtonsoft.Json.Linq;
+using System.Diagnostics;
 namespace apiAssignment.Models
 {
     public class DataManager
     {
-        public DataManager()
+        //create web client object
+        WebClient apiConnection = new WebClient();
+        //define starting API URL
+        string startAPI = "https://www.fishwatch.gov/api/species";
+        //define search variable -- i.e the name we are looking for here
+        string fishName { get; set; }
+        //end point url is combo of start and search variable
+        string apiEndPoint
         {
+            get
+            {
+                return startAPI + fishName;
+            }
         }
+        //constructor required to take on desired fish to find and set it as fishName
+        public DataManager(string fishToFind)
+        {
+            fishName = fishToFind;
+        }
+
+
+        public async Task<List<FishData>> FindFish()
+        {
+            List<FishData> fishList = new List<FishData>();
+
+            string apiString = await apiConnection.DownloadStringTaskAsync(apiEndPoint);
+
+            //pass apiString into a JObject to be parsed into JSON data
+            JObject jsonData = JObject.Parse(apiString);
+
+            //debug to show data is being pulled
+            Debug.WriteLine(jsonData.ToString());
+
+
+
+
+        }
+
+
+
+
+
     }
 }
