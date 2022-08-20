69f2d0f612ee695a5198a71943b73804060bcba9
LuisoNick-FS <87244599+LuisoNick-FS@users.noreply.github.com>
2022-08-20 14:26:06 -0400

    work

---
+++ Luiso_Nick_Alpha/Luiso_Nick_Alpha/SignInPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace Luiso_Nick_Alpha
+{
+    public partial class SignInPage : ContentPage
+    {
+        public SignInPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Luiso_Nick_Alpha/Luiso_Nick_Alpha/SignUpPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace Luiso_Nick_Alpha
+{
+    public partial class SignUpPage : ContentPage
+    {
+        public SignUpPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
